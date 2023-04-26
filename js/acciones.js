b1d74c28b20675176ba3ce8f824c61a879a5c27d
Raule696 <ra.bhamonde@duocuc.cl>
2023-04-26 00:58:11 -0400

    lets

--- js/acciones.js
+++ js/acciones.js
@@ -13,35 +13,3 @@ $(document).ready(function(){
     });
 });
 
-var main = new Splide( '#main-slider', {
-    type       : 'fade',
-    heightRatio: 0.5,
-    pagination : false,
-    arrows     : false,
-    cover      : true,
-  } );
-  
-  var thumbnails = new Splide( '#thumbnail-slider', {
-    rewind          : true,
-    fixedWidth      : 104,
-    fixedHeight     : 58,
-    isNavigation    : true,
-    gap             : 10,
-    focus           : 'center',
-    pagination      : false,
-    cover           : true,
-    dragMinThreshold: {
-      mouse: 4,
-      touch: 10,
-    },
-    breakpoints : {
-      640: {
-        fixedWidth  : 66,
-        fixedHeight : 38,
-      },
-    },
-  } );
-  
-  main.sync( thumbnails );
-  main.mount();
-  thumbnails.mount();
