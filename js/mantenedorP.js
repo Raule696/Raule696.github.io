f9799bd6d42932cab0cb158d3737a4021065aa95
Raule696 <129068738+Raule696@users.noreply.github.com>
2023-05-30 10:23:51 -0400

    sas

---
+++ js/mantenedorP.js
@@ -0,0 +1,47 @@
+// objeto.metodo(json)
+
+$(document).ready(function() {
+
+$("#formulario-mantenedorP").validate({
+  rules: {
+    id: {
+        required: true,
+    },
+    nombre: {
+        required: true,
+    },
+    descripcion: {
+      required: true,
+    },
+    precio: {
+        required: true,
+    },
+    descuentoSub: {
+      required: true,
+    },
+    descuentoOff: {
+      required: true,
+    },
+  }, // --> Fin de reglas
+  messages: {
+    id: {
+        required: "Su Id es un campo requerido",
+    },
+    nombre: {
+        required: "Su nombre es un campo requerido",
+    },
+    descripcion: {
+        required: "La descripción es un campo requerido",
+      },
+    precio: {
+        required: "LEl precio es un campo requerido",
+    },
+    descuentoSub: {
+      required: "El descuentoSub es una campo obligatorio",
+    },
+    descuentoOff: {
+      required: "El descuentoOff es una campo obligatorio",
+    },
+  },
+});
+});
