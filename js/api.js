997d039f72d1544a4dd91c1344750c22059b143e
Raule696 <129068738+Raule696@users.noreply.github.com>
2023-05-30 16:04:00 -0400

    changes

--- js/api.js
+++ js/api.js
@@ -1,25 +1,33 @@
 // Crear funcion ready
 $(document).ready(function () {
-    $("#btnRopa").click(function () {
-        $.get('https://fakestoreapi.com/products',
-            function (data) {
+    $.get('https://fakestoreapi.com/products', // API donde se obtienen los datos
+        function(data){
+            $('#product-item').empty();
 
-                $('#tabla-ropa').empty();
+            $.each(data, function(i, item) { // Recorrer las filas devueltas por la API
+                // Crear el codigo HTML para agegar filas a la tabla usando los campos de cada fila
+                var fila = 
+                    `
+                    <div class="card">
+                        <img src="${item.image}" class="imagen">
+                        <div class="contenido_carta">          
+                            <p>
+                                ${item.price}                                                               
+                            </p>
+                            <p>
+                                ${item.title}
+                            </p>
+                                ${item.description}           
+                            </p>        
+                        </div>
+                    </div> 
+                    `;
+                
+                fila += '</tr>';
 
-                $.each(data, function (i, item) {
-
-                    var fila = '';
-                    fila += '<tr>';
-                    fila += '    <td>' + item.id + '</td>';
-                    fila += '    <td>' + item.title + '</td>';
-                    fila += '    <td><img style="height: 50px; width: 50px" src="' + item.image + '"></td>';
-                    fila +='    <td>' + item.price + '</td>';
-                    fila += '</tr>';
-                    $('#tabla-ropa').append(fila);
-                });
-            }
-        )
-    })
-})
+                $('#product-item').append(fila);   
+            });
+    });
+  });  // Cierre del ready
 
 // Puedes probar otras APIs en http://jsonplaceholder.typicode.com
